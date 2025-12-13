34b66d0f4eefd7207d855da8a7bd8e4cb875fcbb
Gabriel Eduardo Portfólio de Analises e Projetos <gabriel95edu@gmail.com>
2025-12-12 22:13:17 -0300

    Create sheet.js

---
+++ netlify/functions/sheet.js
@@ -0,0 +1,13 @@
+import { google } from "googleapis";
+
+export async function connectSheet() {
+  const auth = new google.auth.GoogleAuth({
+    credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT),
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  });
+
+  const client = await auth.getClient();
+  const sheets = google.sheets({ version: "v4", auth: client });
+
+  return sheets;
+}
